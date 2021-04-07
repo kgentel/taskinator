0071d912ba1ff53c0e50f35ca16ba91150a51192
kgentel <katie.m.gentel@gmail.com>
2021-04-06 21:42:02 -0500

    handle form submission

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,15 +2,12 @@ var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function(event) {
-
   event.preventDefault();
 
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
   listItemEl.textContent = "This is a new task.";
   tasksToDoEl.appendChild(listItemEl);
-
-  console.log(event);
-};
+  };
 
 formEl.addEventListener("submit", createTaskHandler);
